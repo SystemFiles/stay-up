22a583a1f549352c2b276c80c040952db8bfe062
SystemFiles <bensykes12@gmail.com>
2022-01-07 22:31:52 -0500

    (ref): moved client files to stay-up-client project

--- client/src/config/index.js
+++
@@ -1,6 +0,0 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-// Set variables
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5555/api'
-export const API_WEBSOCK_URL = process.env.API_WEBSOCK_URL || 'ws://localhost:5555/api/service/ws'
